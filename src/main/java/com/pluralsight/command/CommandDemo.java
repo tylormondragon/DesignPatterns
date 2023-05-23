a23f235d2882974e88a6c3fc8de7c98a346e4639
tylormondragon <twaitsmo@gmail.com>
2023-05-23 17:23:18 -0500

    ability to control multiple lights in different areas

--- src/main/java/com/pluralsight/command/CommandDemo.java
+++ src/main/java/com/pluralsight/command/CommandDemo.java
@@ -1,16 +1,29 @@
 package com.pluralsight.command;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 //client
 public class CommandDemo {
 
     public static void main(String[] args) {
-        Light light = new Light();
+        Light bedroomLight = new Light();
+        Light kitchenLight = new Light();
         Switch lightSwitch = new Switch();
 
-        Command toggleCommand = new ToggleCommand(light);
+        Command toggleCommand = new ToggleCommand(bedroomLight);
 
         lightSwitch.storeAndExecute(toggleCommand);
-        lightSwitch.storeAndExecute(toggleCommand);
-        lightSwitch.storeAndExecute(toggleCommand);
+//        lightSwitch.storeAndExecute(toggleCommand);
+//        lightSwitch.storeAndExecute(toggleCommand);
+
+        List<Light> lights = new ArrayList<>();
+        lights.add(kitchenLight);
+        lights.add(bedroomLight);
+
+        Command allLightsCommand = new AllLightsCommand(lights);
+
+        lightSwitch.storeAndExecute(allLightsCommand);
     }
 }
--- src/main/java/com/pluralsight/command/Light.java
+++ src/main/java/com/pluralsight/command/Light.java
@@ -5,6 +5,9 @@ public class Light {
 
     private boolean isOn = false;
 
+    public boolean isOn() {
+        return isOn;
+    }
     public void toggle() {
         if(isOn) {
             off();
